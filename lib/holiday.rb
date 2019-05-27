07a1072741c0726b2b71db3687fd30f03b039a37
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-27 22:06:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,7 +40,10 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_hash.map do |season, holiday|
   holiday.map do |holiday_name, supply_for_holiday|
     if holiday_name == :spring
-      supply_for_holiday << supply
+      supply_for_holiday << supply 
+    end
+  end
+end
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
